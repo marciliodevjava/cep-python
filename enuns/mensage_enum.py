a3e37646ecf216b508424c3852ee5ae6722c00b4
marciliodevjava <marciliodevjava@gmail.com>
2024-02-26 22:28:29 -0300

    Update mensage_enum

--- enuns/mensage_enum.py
+++ enuns/mensage_enum.py
@@ -2,4 +2,5 @@ from enum import Enum
 
 
 class CepMessagem(Enum):
+    MENSAGEM_RETORNO_500 = 'Ocorreu um erro na integração'
     MENSAGEM_RETORNO_NONE = 'Não foi possivel verificar o cep solicitado'
