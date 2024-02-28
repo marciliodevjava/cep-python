2646bd2ce4b9aead02e8ac43dbf9621bd7f7f893
marciliodevjava <marciliodevjava@gmail.com>
2024-02-27 21:54:16 -0300

    Update app

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 
+from config.configuracao import db
+from config.segredo import SQLALCHEMY_DATABASE_URI, DATABASE_URI
 from resource.cep_resource import Cep
 
 app = Flask(__name__)
+app.config[DATABASE_URI] = SQLALCHEMY_DATABASE_URI
+
+db.init_app(app)
 api = Api(app)
 
 api.add_resource(Cep, '/cep')
